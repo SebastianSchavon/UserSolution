538e20d409f5c1227070d76c2063e51348c3a6ed
SebastianZen <91784084+SebastianZen@users.noreply.github.com>
2022-05-06 17:49:40 +0200

    Bindable custom passwordbox implemented at loginView (registerView not)

---
+++ User_WPF/Core/Components/BindablePasswordBox.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace User_WPF.Core.Components
+{
+    /// <summary>
+    /// solution for: passwordbox control not bindable 
+    /// userControl passwordbox updates property on passwordChanged
+    /// DependencyProperty property as getter and setter to enable binding
+    /// </summary>
+    public partial class BindablePasswordBox : UserControl
+    {
+        // Using a DependencyProperty as the backing store for PasswordProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty PasswordProperty =
+            DependencyProperty.Register("Password", typeof(string), typeof(BindablePasswordBox), 
+                new PropertyMetadata(string.Empty));
+
+        public string Password
+        {
+            get { return (string)GetValue(PasswordProperty); }
+            set { SetValue(PasswordProperty, value); }
+        }
+
+        public BindablePasswordBox()
+        {
+            InitializeComponent();
+        }
+
+        private void PasswordBox_PasswordChanged(object sender, RoutedEventArgs e)
+        {
+            Password = passwordBox.Password;
+        }
+    }
+}
